d4690526f36953a4f43666d7fde2f4806189c4da
Adam <adamjwittmann@gmail.com>
2024-10-01 14:19:43 -0400

    completed lab 4

--- labs/lab-4/server.py
+++ labs/lab-4/server.py
@@ -51,29 +51,34 @@ with app.app_context():
 
     # LAB 4 --------------------------------------------------
 
-    # # write a query to insert 3 records into the Courses table
-    # query_1 = """ """
-    # db.session.execute(text(query_1))
-    # db.session.commit()
+    # write a query to insert 3 records into the Courses table
+    query_1 = """INSERT INTO "Courses" ("CourseID", "CourseName", "Semester", "Year") VALUES
+    (2, 'Software Development', 'Fall', 2024),
+    (3, 'Ethics', 'Fall', 2024),
+    (4, 'Calculus', 'Fall', 2024);"""
+    db.session.execute(text(query_1))
+    db.session.commit()
 
-    # # write a query to select all records from the Courses table
-    # query_2 = """ """
-    # courses = db.session.execute(text(query_2))
-    # print(f"\n\n----------- Courses Table")
-    # for course in courses:
-    #     print(course)
+    # write a query to select all records from the Courses table
+    query_2 = """SELECT * from "Courses"; """
+    courses = db.session.execute(text(query_2))
+    print(f"\n\n----------- Courses Table")
+    for course in courses:
+        print(course)
 
-    # # write a query to update 1 record in the Courses table
-    # query_3 = """ """
-    # db.session.execute(text(query_3))
-    # db.session.commit()
+    # write a query to update 1 record in the Courses table
+    query_3 = """UPDATE "Courses"
+    SET "CourseName"= 'Database Management', "Semester" = 'Spring'
+    WHERE "CourseID" = 3;"""
+    db.session.execute(text(query_3))
+    db.session.commit()
 
-    # # write a query to delete 1 record in the Courses table
-    # query_4 = """ """
-    # db.session.execute(text(query_4))
-    # db.session.commit()
+    # write a query to delete 1 record in the Courses table
+    query_4 = """DELETE FROM "Courses" WHERE "CourseID"='1';"""
+    db.session.execute(text(query_4))
+    db.session.commit()
 
-    # print(f"\n\n----------- Courses Table")
-    # courses = db.session.execute(text(query_2))
-    # for course in courses:
-    #     print(course)
+    print(f"\n\n----------- Courses Table")
+    courses = db.session.execute(text(query_2))
+    for course in courses:
+        print(course)
